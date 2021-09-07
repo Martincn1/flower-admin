bb05ce6c220411daa9c5806cffefcdc67cd2dfda
kena <18329177975@163.com>
2021-09-07 19:49:51 +0800

    fix: 表格调整

--- src/views/lottery-manage/prize-manage/index.vue
+++ src/views/lottery-manage/prize-manage/index.vue
@@ -28,7 +28,6 @@
 
 <script>
 import ModifyDialog from './components/ModifyDialog.vue'
-import OperateBtn from './components/OperateBtn.vue'
 import SearchBar from './components/SearchBar.vue'
 
 import listMixins from 'mixins/list-mixins'
@@ -46,7 +45,6 @@ import { getPrizeList, addPrizeInfo, updatePrizeInfo, deletePrizeInfo } from 'ap
 export default {
   components: {
     ModifyDialog,
-    OperateBtn,
     SearchBar
   },
   mixins: [listMixins],
