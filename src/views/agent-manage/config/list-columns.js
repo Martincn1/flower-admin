c14ed3ae4121fbbeff268a504bc110146ca69288
kena <18329177975@163.com>
2021-09-08 20:40:39 +0800

    fix: 代码优化

--- src/views/agent-manage/config/list-columns.js
+++ src/views/agent-manage/config/list-columns.js
@@ -1,33 +1,31 @@
-import {
-  basicColumnField,
-  switchColumnField,
-  optionColumnField
-} from 'config/columns/index'
+import { formatNormalize } from 'utils/filter'
+
+import ColumnFieldClass from 'config/columns'
 
 import { COMON_STATUS_ENUM } from 'enums/common/index'
 
-export default ({ modifyHandler, changeStatus }) => ([
-  basicColumnField({ prop: 'id', label: '编号' }),
-  basicColumnField({ prop: 'name', label: '单位名称' }),
-  basicColumnField({ prop: 'phone', label: '负责人联系方式' }),
-  basicColumnField({ prop: 'userName', label: '负责人姓名' }),
-  basicColumnField({ prop: 'account', label: '银行卡' }),
-  basicColumnField({ prop: 'branch', label: '支行' }),
-  basicColumnField({ prop: 'idNumber', label: '身份证' }),
-  basicColumnField({ prop: 'money', label: '当前可提现金额' }),
-  basicColumnField({ prop: 'historyMoney', label: '历史可提现金额' }),
-  basicColumnField({ prop: 'proportion', label: '历史可提现金额' }),
-  basicColumnField({ prop: 'tax', label: '税' }),
-  switchColumnField({
-    prop: 'status',
-    label: '是否禁用',
+export default ({ modifyHandler, changeStatus }) => ColumnFieldClass.generateField([
+  { type: 'base', prop: 'id', label: '编号', formatter: (row) => formatNormalize(row.id) },
+  { type: 'base', prop: 'name', label: '单位名称', formatter: (row) => formatNormalize(row.name) },
+  { type: 'base', prop: 'phone', label: '负责人联系方式', formatter: (row) => formatNormalize(row.phone) },
+  { type: 'base', prop: 'userName', label: '负责人姓名', formatter: (row) => formatNormalize(row.userName) },
+  { type: 'base', prop: 'account', label: '银行卡', formatter: (row) => formatNormalize(row.account) },
+  { type: 'base', prop: 'branch', label: '支行', formatter: (row) => formatNormalize(row.branch) },
+  { type: 'base', prop: 'idNumber', label: '身份证', formatter: (row) => formatNormalize(row.idNumber) },
+  { type: 'base', prop: 'money', label: '当前可提现金额', formatter: (row) => formatNormalize(row.money) },
+  { type: 'base', prop: 'historyMoney', label: '历史可提现金额', formatter: (row) => formatNormalize(row.historyMoney) },
+  { type: 'base', prop: 'proportion', label: '历史可提现金额', formatter: (row) => formatNormalize(row.proportion) },
+  { type: 'base', prop: 'tax', label: '税', formatter: (row) => formatNormalize(row.tax) },
+  { type: 'switch', prop: 'status', label: '是否禁用', slots: {
     activeValue: COMON_STATUS_ENUM.NORMAL,
-    inactiveValue: COMON_STATUS_ENUM.DISABLED },
-  { onEvent: changeStatus }),
-  optionColumnField({ label: '操作' }, [{
+    inactiveValue: COMON_STATUS_ENUM.DISABLED,
+    event: changeStatus
+  }},
+  { type: 'operate', label: '操作', slots: [{
+    field: 'modifyBtn',
     type: 'primary',
     size: 'mini',
-    txt: '修改',
+    title: '修改',
     onEvent: modifyHandler
-  }])
+  }] }
 ])
--- src/views/order-manage/config/list-columns.js
+++ src/views/order-manage/config/list-columns.js
@@ -1,12 +1,4 @@
 import {
-  basicColumnField,
-  imageColumnField,
-  tagColumnField,
-  areaColumnField,
-  optionColumnField
-} from 'config/columns/index'
-
-import {
   PAID_STATUS_STYLE,
   PAID_STATUS_MAP,
   SHIP_STATUS_MAP,
@@ -15,36 +7,33 @@ import {
   ORDER_STATUS_STYLE
 } from 'enums/order-manage/index'
 
-export default ({ modifyHandler }) => ([
-  basicColumnField({ prop: 'id', label: '编号' }),
-  tagColumnField(
-    { prop: 'status', label: '支付状态' },
-    { styleMap: PAID_STATUS_STYLE, fieldMap: PAID_STATUS_MAP }
-  ),
-  basicColumnField({ prop: 'realAmount', label: '实付商品总金额' }),
-  basicColumnField({ prop: 'orderNo', label: '订单号' }),
-  basicColumnField({ prop: 'payOrderNo', label: '三方支付单号' }),
-  basicColumnField({ prop: 'code', label: '物流单号' }),
-  basicColumnField({ prop: 'address', label: '地址' }),
-  basicColumnField({ prop: 'name', label: '购买用户' }),
-  basicColumnField({ prop: 'phone', label: '用户手机号' }),
-  areaColumnField({ prop: 'area', label: '地区' }),
-  tagColumnField(
-    { prop: 'isBiu', label: '是否发货' },
-    { styleMap: SHIP_STATUS_STYLE, fieldMap: SHIP_STATUS_MAP }
-  ),
-  tagColumnField(
-    { prop: 'orderStatus', label: '订单状态' },
-    { styleMap: ORDER_STATUS_STYLE, fieldMap: ORDER_STATUS_MAP }
-  ),
-  basicColumnField({ prop: 'preferentialAmount', label: '优惠金额' }),
-  basicColumnField({ prop: 'goodsName', label: '商品名称' }),
-  basicColumnField({ prop: 'goodsMoney', label: '	商品价格' }),
-  imageColumnField({ prop: 'goodsImage', label: '商品图' }),
-  optionColumnField({ label: '操作' }, [{
-    type: 'primary',
-    size: 'mini',
-    txt: '修改',
-    onEvent: modifyHandler
-  }])
+import ColumnFieldClass from 'config/columns'
+
+import { formatNormalize } from 'utils/filter'
+
+import { modifyBtn } from 'config/operate-button'
+
+export default ({ modifyHandler }) => ColumnFieldClass.generateField([
+  { type: 'base', prop: 'id', label: '编号' },
+  { type: 'tag', prop: 'status', label: '支付状态', slots: {
+    styleMap: PAID_STATUS_STYLE, fieldMap: PAID_STATUS_MAP
+  }},
+  { type: 'base', prop: 'realAmount', label: '实付商品总金额' },
+  { type: 'base', prop: 'orderNo', label: '订单号' },
+  { type: 'base', prop: 'payOrderNo', label: '三方支付单号' },
+  { type: 'base', prop: 'code', label: '物流单号' },
+  { type: 'base', prop: 'address', label: '地址' },
+  { type: 'base', prop: 'name', label: '购买用户' },
+  { type: 'base', prop: 'phone', label: '用户手机号' },
+  { type: 'base', prop: 'area', label: '地区', formatter: (row) => {
+    const { p, c, d } = row
+    return formatNormalize(`${p}-${c}-${d}`)
+  } },
+  { type: 'base', prop: 'isBiu', label: '是否发货', slots: { styleMap: SHIP_STATUS_STYLE, fieldMap: SHIP_STATUS_MAP }},
+  { type: 'base', prop: 'orderStatus', label: '订单状态', slots: { styleMap: ORDER_STATUS_STYLE, fieldMap: ORDER_STATUS_MAP }},
+  { type: 'base', prop: 'preferentialAmount', label: '优惠金额' },
+  { type: 'base', prop: 'goodsName', label: '商品名称' },
+  { type: 'base', prop: 'goodsMoney', label: '	商品价格' },
+  { type: 'image', prop: 'goodsImage', label: '商品图' },
+  { type: 'operate', label: '操作', slots: [modifyBtn(modifyHandler)] }
 ])
